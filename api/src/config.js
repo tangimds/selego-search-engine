85ba3aee545ab0712b072eb8b30bc10b59abdc60
Tangi Mendes <tangi.mendes@benevolt.fr>
2020-10-07 15:13:15 +0200

    fix

--- api/src/config.js
+++ api/src/config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = process.env.DB_ENDPOINT || "mongodb+srv://tangimds:Showtime@cluster0.xpgyv.mongodb.net/lorem?retryWrites=true&w=majority";
+const MONGO_URL = process.env.DB_ENDPOINT || "";
 const PORT = process.env.PORT || 8080;
 const secret = process.env.SECRET || "not-so-secret";
 const APP_URL = process.env.DEV === "true" ? "http://localhost:8083" : "http://localhost:8083";
